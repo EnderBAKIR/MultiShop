f18c08e8ba138c1a7c75bdfaf9972f4256922edc
Ender Bakır <enderyx_123@hotmail.com>
2024-04-03 20:20:19 +0300

    Added DatabaseSettings.cs => Catalog Microservice/Settings

---
+++ Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CategoryCollectionName { get; set; }
+
+        public string ProductCollectionName { get; set; }
+
+        public string ProductDetailtCollectionName { get; set; }
+
+        public string ProductImageCollectionName { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
