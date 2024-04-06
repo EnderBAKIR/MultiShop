594511842798ab5dc891938055a8c978f5addd0c
Ender Bakır <enderyx_123@hotmail.com>
2024-04-06 12:51:20 +0300

    Added Coupon Dtos => Discount Microservice / Dtos

---
+++ Services/Discount/MultiShop.Discount/DTOs/CreateCouponDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Discount.DTOs
+{
+    public class CreateCouponDto
+    {
+        public string Code { get; set; }
+
+        public int Rate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime ValidDate { get; set; }
+    }
+}
---
+++ Services/Discount/MultiShop.Discount/DTOs/ResultCouponDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiShop.Discount.DTOs
+{
+    public class ResultCouponDto
+    {
+        public int CouponId { get; set; }
+
+        public string Code { get; set; }
+
+        public int Rate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime ValidDate { get; set; }
+    }
+}
