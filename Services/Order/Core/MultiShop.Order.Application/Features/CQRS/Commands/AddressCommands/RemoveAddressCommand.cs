2ffac88099b9b5dfd4a19ade1df6ea63ee9479fd
Ender Bakır <enderyx_123@hotmail.com>
2024-04-08 09:59:43 +0300

    Added RemoveAddressCommand=> Order Microservice / CQRS

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+    public class RemoveAddressCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveAddressCommand(int id)
+        {
+            Id = id;
+        }
+
+        
+    }
+}
