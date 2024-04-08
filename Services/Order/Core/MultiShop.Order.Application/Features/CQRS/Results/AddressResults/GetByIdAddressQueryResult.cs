d559d854df29a9d676b54b50debd53e1c13de368
Ender Bakır <enderyx_123@hotmail.com>
2024-04-08 09:48:49 +0300

    Added GetByIdAddressQueryResult=> Order Microservice / CQRS

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/AddressResults/GetByIdAddressQueryResult.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Results.AddressResults
+{
+    public class GetByIdAddressQueryResult
+    {
+        public int AddressId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string District { get; set; }
+
+        public string City { get; set; }
+
+        public string Detail { get; set; }
+    }
+}
